2582b9de8f478454f0435ad630579f2bfbbf6d24
1131757183 <1131757183@qq.com>
2019-11-07 15:36:43 +0800

    TCP/IP网络编程

---
+++ TCP_IP_Network_Programming_Codes/uecho_client.cpp
@@ -0,0 +1,93 @@
+﻿#include"syscheck.h"
+
+#define BUF_SIZE	1024
+
+void error_handling(const char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+int main(int argc, char* argv[]) {
+#ifdef __linux
+	int sock;
+	char message[BUF_SIZE];
+	int str_len;
+	socklen_t adr_sz;
+	struct sockaddr_in serv_adr, from_adr;
+
+	if (argc != 3) {
+		printf("Usage : %s <IP> <port>\n", argv[0]);
+		exit(1);
+	}
+
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock == -1) {
+		error_handling("socket() error");
+	}
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
+	serv_adr.sin_port = htons(atoi(argv[2]));
+
+	while (1) {
+
+		fputs("Insert message(q to quit):", stdout);
+		fgets(message, sizeof(message), stdin);
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+		sendto(sock, message, strlen(message), 0, (struct sockaddr *)&serv_adr, sizeof(serv_adr));//一次性发送完毕（不保证完全接收）
+		adr_sz = sizeof(from_adr);
+		str_len = recvfrom(sock, message, BUF_SIZE, 0, (struct sockaddr *)&from_adr, &adr_sz);
+		message[str_len] = 0;
+
+		printf("Message from server: %s", message);
+	}
+	close(sock);
+	return 0;
+#endif
+
+#ifdef _WIN32
+	WSADATA wsaData;
+	SOCKET sock;
+	char message[BUF_SIZE];
+	int strLen;
+
+	SOCKADDR_IN servAdr;
+	if (argc != 3) {
+		printf("Usage: %s <IP><port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		error_handling("WSAStartup() error!");
+	sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (sock == INVALID_SOCKET)
+		error_handling("socket() error!");
+
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.S_un.S_addr = inet_addr(argv[1]);
+	servAdr.sin_port = htons(atoi(argv[2]));
+
+	connect(sock, (SOCKADDR*)&servAdr, sizeof(servAdr));
+
+	while (1) {
+		fputs("Insert message(q to quit): ", stdout);
+		fgets(message, sizeof(message), stdin);
+		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
+			break;
+
+		send(sock, message, strlen(message), 0);
+		strLen = recv(sock, message, sizeof(message) - 1, 0);
+		message[strLen] = 0;
+		printf("Message from server: %s", message);
+	}
+	closesocket(sock);
+	WSACleanup();
+	return 0;
+#endif
+
+}
+
---
+++ TCP_IP_Network_Programming_Codes/uecho_server.cpp
@@ -0,0 +1,96 @@
+﻿//UDP迭代回声服务器端 / 客户端：
+//服务器端在同一时刻只与一个客户端相连， 并提供回声服务。
+//服务器端依次向5个客户端提供服务并退出。
+//客户端接收用户输人的字符串并发送到服务器端。
+//服务器端将接收的字符串数据传回客户端，即“回声”。
+//服务器端与客户端之间的字符串回声一直执行到客户端输人Q为止。
+
+#include"syscheck.h"
+
+#define BUF_SIZE	1024
+
+void error_handling(const char* message) {
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
+
+int main(int argc, char* argv[]) {
+#ifdef __linux
+	int serv_sock;
+	char message[BUF_SIZE];
+	int str_len;
+	struct sockaddr_in serv_adr;
+	struct sockaddr_in clnt_adr;
+	socklen_t clnt_addr_sz;
+
+	if (argc != 2) {
+		printf("Usage : %s <port>\n", argv[0]);
+		exit(1);
+	}
+
+	serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (serv_sock == -1) {
+		error_handling("socket() error");
+	}
+
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1) {
+		error_handling("bind() error");
+	}
+
+	while (1) {
+
+		clnt_addr_sz = sizeof(clnt_adr);
+		str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_addr_sz);
+		sendto(serv_sock, message, str_len, 0, (struct sockaddr*)&clnt_adr, clnt_addr_sz);
+
+	}
+	close(serv_sock);
+	return 0;
+#endif
+
+#ifdef _WIN32
+	WSADATA wsaData;
+	SOCKET servSock;
+	char message[BUF_SIZE];
+	int strLen;
+	int clntAdrSz;
+
+	SOCKADDR_IN servAdr, clntAdr;
+	if (argc != 2) {
+		printf("Usage : %s<port>\n", argv[0]);
+		exit(1);
+	}
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		error_handling("WSAStartup() error!");
+
+	servSock = socket(PF_INET, SOCK_DGRAM, 0);
+	if (servSock == INVALID_SOCKET)
+		error_handling("UDP socket creation error!");
+
+	memset(&servAdr, 0, sizeof(servAdr));
+	servAdr.sin_family = AF_INET;
+	servAdr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
+	servAdr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(servSock, (SOCKADDR*)&servAdr, sizeof(servAdr)) == SOCKET_ERROR)
+		error_handling("bind() error!");
+
+	while (1) {
+		clntAdrSz = sizeof(clntAdr);
+		strLen = recvfrom(servSock, message, BUF_SIZE, 0, (SOCKADDR*)&clntAdr, &clntAdrSz);
+		sendto(servSock, message, strLen, 0, (SOCKADDR*)&clntAdr, sizeof(clntAdr));
+	}
+	closesocket(servSock);
+	WSACleanup();
+	return 0;
+#endif
+
+}
+
